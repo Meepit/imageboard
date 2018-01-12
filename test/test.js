599b770a2974bb550247bf2b492f4e84a1a5cca5
Lars <larsfinlay@googlemail.com>
2018-01-12 15:55:04 +0000

    testing for form

--- test/test.js
+++ test/test.js
@@ -10,7 +10,25 @@ describe('Feature testing', function(){
     browser.visit('/', done);
   })
 
-  it('Can see express', function(){
-    browser.assert.text('title', 'Express');
+  it('Can see a form', function(){
+    browser.assert.text('#form', 'title: description: ');
   })
-})
+
+  describe('Can submit a thread', function() {
+
+    before(function(done) {
+      browser
+        .fill('#title', 'Gaming')
+        .fill('#description', 'general discussion 2018 gaming')
+        .pressButton('Post', done);
+    });
+
+    it('Can see post has been made', function() {
+      browser.assert
+      .text('#title', 'Gaming')
+      .text('#description', 'general discussion 2018 gaming');
+    });
+
+  });
+
+});
